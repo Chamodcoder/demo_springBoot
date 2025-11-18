59574b7a1a815263c8f997f72a243e3c08b6c9bd
R.G. Chamod chathuranga <chamodchathuranga2002@gmail.com>
2025-11-18 18:45:28 +0530

    Configure application properties and update project setup. Refine project configuration and repository setup

--- src/main/java/com/example/demo/repo/UserRepo.java
+++ src/main/java/com/example/demo/repo/UserRepo.java
@@ -1,7 +1,9 @@
 package com.example.demo.repo;
 
 import org.springframework.stereotype.Repository;
+import com.example.demo.model.User;
+import org.springframework.data.jpa.repository.JpaRepository;
 
 @Repository
-public interface UserRepo extends {
+public interface UserRepo extends JpaRepository<User,Integer>{
 }
