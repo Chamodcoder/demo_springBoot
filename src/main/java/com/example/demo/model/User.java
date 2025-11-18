8a6141704afd01e8d0b13377e6b13cbd6b90d39b
R.G. Chamod chathuranga <chamodchathuranga2002@gmail.com>
2025-11-18 18:08:43 +0530

    Configure application properties and update project setup

---
+++ src/main/java/com/example/demo/model/User.java
@@ -0,0 +1,16 @@
+package com.example.demo.model;
+
+import jakarta.persistence.Entity;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@NoArgsConstructor
+@AllArgsConstructor
+@Data
+public class User {
+    @id
+    private int id;
+    private String name;
+}
