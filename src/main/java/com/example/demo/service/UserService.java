d6a6b84ca4ba728900b5461a335b9901d5d911b8
R.G. Chamod chathuranga <chamodchathuranga2002@gmail.com>
2025-11-19 10:22:52 +0530

    Configure application properties and update project setup. Refine project configuration and repository setup

--- src/main/java/com/example/demo/service/UserService.java
+++ src/main/java/com/example/demo/service/UserService.java
@@ -1,4 +1,19 @@
 package com.example.demo.service;
 
+import jakarta.transaction.Transactional;
+import org.modelmapper.ModelMapper;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import com.example.demo.repo.UserRepo;
+
+@Transactional
+@Service
 public class UserService {
+    @Autowired
+    private UserRepo userRepo;
+
+    @Autowired
+    private ModelMapper modelMapper;
+
+    public List
 }
